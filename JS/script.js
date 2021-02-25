38d2984b4a7f99b75144c6a50661257399d9ff56
Gr-Stanislav <grstas1990@gmail.com>
2021-02-25 22:53:06 +0200

    area of ​​a triangle

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,11 @@
-const num1 = prompt ("Первое число");
-const num2 = prompt ("Второе число");
-const num3 = prompt ("Третье число");
+const a = parseInt(prompt ("Ширина триугольникао:"));
+const h = parseInt(prompt ("Высота триугольника:"));
+let s;
+
 
 function define() {
-    alert (num1 + num2 + num3);
+    s = (a * h) / 2;
+    alert ("Площадь треугольника: " + s + " см. куб");
 }
 
 define();
